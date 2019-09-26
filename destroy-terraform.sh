0ed8f02dcaba8b58b85848a2dc3794ebdc99fba2
Administrator <admin@example.com>
2019-09-26 19:22:27 +0300

    remove all

--- destroy-terraform.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Use this script to destroy cluster via terraform command
-terraform state rm kubernetes_namespace.spinnaker
-terraform state rm module.postgres.google_sql_user.default
-terraform destroy
