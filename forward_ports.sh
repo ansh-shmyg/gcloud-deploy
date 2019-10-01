07c013e67a254c3dcf36f55f735f13d63ce47a50
Andrii Sh <ash@ansh-work>
2019-10-01 21:01:16 +0300

    add grafana configuration; add --live-manifest-calls for haliard spinnaker

--- forward_ports.sh
+++ forward_ports.sh
@@ -19,7 +19,7 @@ echo "spin-deck: http://localhost:9000"
 
 GRAF_POD=$(kubectl -n istio-system get pod -l app=grafana --kubeconfig=kubeconfig -ojsonpath='{.items[0].metadata.name}')
 kubectl -n istio-system port-forward ${GRAF_POD} 3000 --kubeconfig=kubeconfig >> /dev/null 2>&1 &
-echo "grafana: http://localhost:8084"
+echo "grafana: http://localhost:3000"
 
 PROM_POD=$(kubectl -n istio-system get pod -l app=prometheus --kubeconfig=kubeconfig -ojsonpath='{.items[0].metadata.name}')
 kubectl -n istio-system port-forward ${PROM_POD} 9090 --kubeconfig=kubeconfig >> /dev/null 2>&1 &
